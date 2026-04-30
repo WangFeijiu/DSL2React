61f4289961881bb71b7faf81653d843cf9f7d2d4
joe <1762509314@qq.com>
2026-04-30 14:42:02 +0800

    fix: update ESLint and TypeScript config for latest versions
    - Migrate to eslint.config.js (ESLint v10)
    - Update moduleResolution to bundler
    - All tests passing

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-console': 'warn',
+    },
+  },
+];
